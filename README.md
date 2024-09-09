02151c84308d577fa89689e9700c0d5e7bcfdb65
Peter Weis Hansen <magni-ps@hotmail.com>
2024-09-09 22:54:16 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Data scraper for the game Europa Universalis 4, or mods thereof
+A program designed to scrape a series of data for the game Europa Universalis 4 for easy analysis of various characteristics. 
+
+The primary functionality is to create a pseudo-relational database using classes, allowing a user to easily determine various properties of classes in relation to another class. For example, how many provinces on a specific continent are owned by countries whose culture is different from that of the province?
---
+++ main.py
@@ -0,0 +1,56 @@
+import functions_scraping as scrape
+from functions import save_info_as_csv
+from functions_map import create_province_map, get_neighbors, country_map
+from classes import *
+import os
+
+#%% TO-DO
+# Colors for cultures and religions.
+# Technology groups?
+# Country flags
+# Tribal lands?
+# Rivers mapping.
+# Adjacencies from straits and the like.
+
+#%% Defines.
+DEFINES = {}
+DEFINES['MAP_PROVINCES'] = True 
+DEFINES['OUTPUT'] = True # Should always be false if the above is false.
+DEFINES['ORIGIN_PATH'] = os.getcwd()
+DEFINES['MAIN_PATH'] = r'C:\Users\Peter\Documents\Paradox Interactive\Europa Universalis IV\mod\anbennar-eu4-ovdal-tungr-build'
+
+
+#%% Actually running stuff.
+if __name__ == "__main__":
+    path = DEFINES['MAIN_PATH']
+    
+    scrape.load_cultures(path)
+    print("Cultures loaded.")
+    scrape.load_religions(path)
+    print("Religions loaded.")
+    scrape.load_countries(path)
+    print("Countries loaded.")
+    scrape.load_provinces(path)
+    print("Provinces loaded.")
+    scrape.assign_areas_etc(path)
+    print("Areas etc. set.")
+    scrape.assign_trade_nodes(path)
+    print("Trade nodes assigned.")
+    scrape.load_and_assign_terrain(path)
+    print("Terrain assidned.")
+    scrape.load_and_assign_climate(path)
+    print("Climate assigned.")
+    scrape.misc_cleanup()
+    print("Cleanup.")
+    
+    if DEFINES['MAP_PROVINCES']:
+        print("Creating the province map.")
+        province_map, image = create_province_map(DEFINES['MAIN_PATH'])
+        # Warning: the lines below adds a minute or so of runtime.
+        print("Determining neighbors.")
+        get_neighbors(province_map) # Determines land and sea neighbors for all provinces.
+        country_map(image, province_map, DEFINES['ORIGIN_PATH']) # Creates a country-view map for testing purposes, called testing.png
+    
+    if DEFINES['MAP_PROVINCES'] and DEFINES['OUTPUT']:
+        save_info_as_csv('output')
+        
